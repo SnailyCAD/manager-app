55a93094f4a4d69fa629f49de4df2cac3e04898c
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-01-13 19:54:31 +0100

    :hammer: chore: minor refactor

--- src/renderer/AppRenderer.tsx
+++ src/renderer/AppRenderer.tsx
@@ -26,17 +26,6 @@ const GlobalStyle = createGlobalStyle`
   #app {
     min-height: 100vh;
   }
-
-  @font-face {
-    font-family: 'icon';
-    src: url('fonts/icon.eot?wb6ufj');
-    src: url('fonts/icon.eot?wb6ufj#iefix') format('embedded-opentype'),
-      url('fonts/icon.ttf?wb6ufj') format('truetype'),
-      url('fonts/icon.woff?wb6ufj') format('woff'),
-      url('fonts/icon.svg?wb6ufj#icomoon') format('svg');
-    font-weight: normal;
-    font-style: normal;
-  }
 `;
 
 window.addEventListener("load", () => {
